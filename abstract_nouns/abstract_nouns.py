27068cab39ee8bb4b362de7f11e3e6de9222e778
cmd16 <catherine.dejager10@gmail.com>
2018-01-09 16:24:28 -0500

    Moved code into function

--- abstract_nouns/abstract_nouns.py
+++ abstract_nouns/abstract_nouns.py
@@ -7,17 +7,21 @@ Get the abstract nouns in a corpus by searching for various word endings as foun
 Preconditions: infilename refers to a .txt file containing a word list created from AntConc
 """
 
+"""
 infilename = input("Infilename: ")
 infile = open(infilename,'r')
+"""
 
+# global variables
 abstract_noun_list = ["ion", "ions", "ity", "itys", "ness", "nesses", "dom", "doms", "ment", "ments"
                       "age", "ance", "ence", "ce", "cy", "ess", "esse", "head", "hood", "ice", "ise", "ism",
                       "ry", "tude", "ty", "ure"]
 
-linenum = 0
-for line in infile:
-    if linenum < 3:  # first 3 lines contain metadata
-        continue
-    data = str(line).split()
-
-    linenum += 1
+# function definitions
+def get_abstract_nouns(infile=open(input("Infilename: "))):
+    linenum = 0
+    for line in infile:
+        if linenum < 3:  # first 3 lines contain metadata
+            continue
+        data = str(line).split()
+        linenum += 1
