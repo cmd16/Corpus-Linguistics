104993b57ba707c9a2ab42a976277306011e4e42
cmd16 <catherine.dejager10@gmail.com>
2019-05-10 14:25:56 -0400

    Use nltk instead of pandas

--- Standalone corpus linguistics/hmm_backoff.py
+++ Standalone corpus linguistics/hmm_backoff.py
@@ -5,8 +5,7 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from nltk import FreqDist
-from nltk.tokenize import word_tokenize
+from nltk import FreqDist, word_tokenize, ngrams
 np.random.seed(seed=234)
 
 def iterator_from_idfile(idfile, path, end=".txt"):
@@ -151,8 +150,63 @@ def base_freq(og, sums, ngrams):
 # For use in later functions so as not to re-calculate multiple times:
 # bf = base_freq(number_of_onegrams(sums))
 
+def get_ngrams_from_str(text, lower=1, upper=5, case_sensitive=False):
+    ngram_dict = dict()
+    if case_sensitive:
+        words = [word for word in word_tokenize(text) if
+                 word.isalpha() or word.replace("'", "").isalpha()]
+    else:
+        words = [word for word in word_tokenize(text.lower()) if
+                 word.isalpha() or word.replace("'", "").isalpha()]
+    for i in range(lower, upper + 1):
+        ngram_dict[i] = FreqDist(ngrams(words, i))
+    return ngram_dict
+
+def get_ngrams_from_file(filename, lower=1, upper=5, case_sensitive=False):
+    with open(filename) as f_in:
+        return get_ngrams_from_str(f_in.read(), lower=lower, upper=upper, case_sensitive=case_sensitive)
+
+def get_ngrams_from_idlist(idlistname, in_path, end=".txt", lower=1, upper=5, case_sensitive=False):
+    with open(idlistname) as f_in:
+        ng = dict()
+        for i in range(lower, upper + 1):
+            ng[i] = FreqDist()
+        for line in f_in:
+            name = os.path.join(in_path, line.strip() + end)
+            local_ng = get_ngrams_from_file(name, lower=lower, upper=upper, case_sensitive=case_sensitive)
+            for i in range(lower, upper + 1):
+                ng[i].update(local_ng[i])
+    return ng
+
+def write_ngrams_to_file(ngram_dict, basename, out_path):
+    for key in ngram_dict:
+        print(basename, key, "grams")
+        with open("%s/%d_grams/%s" % (out_path, key, basename), "w") as f_out:
+            fd = ngram_dict[key]
+            types = len(fd)
+            tokens = fd.N()
+            f_out.write("#Ngram types: %d\n" % types)  # unique words
+            f_out.write("#Word tokens: %d\n" % tokens)  # total words
+            rank = 1
+            for tup in fd.most_common():
+                word = tup[0]
+                freq = tup[1]
+                norm = freq * 1000000 / tokens
+                f_out.write("%d\t%s\t%d\t%f" % (rank, word, freq, norm))
+                rank += 1
+
+def fanfic_ngrams_to_file():
+    proj_dir = "/Volumes/2TB/Final_project"
+    fanfic_dir = os.path.join(proj_dir, "Fanfic_all")
+    idlist_dir = os.path.join(proj_dir, "Fanfic lists")
+    for idlistname in os.listdir(idlist_dir):
+        if idlistname.endswith(".txt"):
+            print(idlistname, "reading")
+            ng = get_ngrams_from_idlist(idlistname, in_path=fanfic_dir)
+            print(idlistname, "writing")
+            write_ngrams_to_file(ng, basename=idlistname, out_path=proj_dir)
+
 if __name__ == "__main__":
     proj_dir = "/Volumes/2TB/Final_project"
-    print("Getting dataframes")
-    write_ngram_df_fanfic()
+    fanfic_ngrams_to_file()
     # fanfic_ngrams_from_df()
